3016f151678c8b7e6846ad0cce8746db555dc747
DevDani <86773015+dgiaimo996@users.noreply.github.com>
2024-06-12 15:09:31 +0200

    git a cazzo
    git a cazzo

--- src/api/betting/betting.module.ts
+++ src/api/betting/betting.module.ts
@@ -1,6 +1,6 @@
-import { Elysia } from "elysia";
+import {Elysia} from "elysia";
 
-const router = new Elysia({ prefix: '/betting' });
+const router = new Elysia({prefix: '/betting'});
 
 let Bets: {
     id: string;
@@ -17,7 +17,7 @@ const generateBetId = () => {
     let bet;
     do {
         id = Math.random().toString(36).substring(2, 6);
-        bet = Bets.find(({ id: idToCheck }) => idToCheck === id);
+        bet = Bets.find(({id: idToCheck}) => idToCheck === id);
     } while (!!bet);
     return id;
 }
@@ -25,18 +25,27 @@ const generateBetId = () => {
 const saveBets = () => {
     require('fs').writeFileSync(require('path').join(__dirname, '../../../data/betting.json'), JSON.stringify(Bets));
 }
-
-router.get('/:id', ({ params: { id } }) => {
+router.get('/bet', () => {
+    return Bets;
+});
+router.get('/:id', ({params: {id}}) => {
     const bet = Bets.find((bet: { id: string; }) => bet.id === id);
     if (!bet) {
         throw new Error('Bet not found');
     }
     return bet;
 });
-
-router.post('/:id', ({ params: { id }, body: { name, phone, amount, bets, sport, password, claimed } }: {
+router.post('/:id', ({params: {id}, body: {name, phone, amount, bets, sport, password, claimed}}: {
     params: { id: string; };
-    body: { bets: string; sport: string; name: string; phone: string; amount: number; password: string; claimed: boolean;};
+    body: {
+        bets: string;
+        sport: string;
+        name: string;
+        phone: string;
+        amount: number;
+        password: string;
+        claimed: boolean;
+    };
 }) => {
     console.log(password, process.env.BETTING_PASSWORD)
     if (password !== process.env.BETTING_PASSWORD) {
@@ -74,5 +83,30 @@ router.post('/:id', ({ params: { id }, body: { name, phone, amount, bets, sport,
     };
 });
 
+router.post('/:id/claim', ({params: {id}}: {
+    params: { id: string; };
+}) => {
+    const bet = Bets.find((bet: { id: string; }) => bet.id === id);
+    if (!bet) {
+        throw new Error('Bet not found');
+    }
+    bet.claimed = true;
+    saveBets();
+    return {
+        id: bet.id
+    };
+});
+
+router.post('/checkLogin', ({body: {username, password}}: {
+    body: { username: string; password: string; };
+}) => {
+    if (username === process.env.BETTING_USERNAME) {
+        if (password === process.env.BETTING_PASSWORD) {
+            return true;
+        }
+    } else {
+        return false;
+    }
+});
 
 export default router;
